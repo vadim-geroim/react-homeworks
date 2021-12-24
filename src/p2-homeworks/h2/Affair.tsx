e2390839ea209ff2db0f93170c387e57b67d9f7b
vgeroim <vgeroim.qa@gmail.com>
2021-12-23 19:42:18 -0600

    Fix delete affair button, display affair data on the web page

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
+import { AffairType } from "./HW2";
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
-}
+  // key не нужно типизировать
+  affair: AffairType;
+  deleteAffairCallback: any; // need to fix any
+};
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+  const deleteCallback = () => {
+    props.deleteAffairCallback(props.affair._id);
+  };
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <span>{props.affair._id}</span>&nbsp;
+        <span>{props.affair.name}</span>&nbsp;
+        <span>{props.affair.priority}</span>&nbsp;
+        <button onClick={deleteCallback}>X</button>
+      </div>
+    </div>
+  );
 }
 
-export default Affair
+export default Affair;
