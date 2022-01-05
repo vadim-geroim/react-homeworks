3eb6b87e347ea46f4a5d21760e73f62c91a0be58
vgeroim <vgeroim.qa@gmail.com>
2022-01-04 20:31:49 -0600

    Use types instead of any, implement methods addUser, setNameCallback, show error message on wrong entry

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import s from './Greeting.module.css'
+import React, { ChangeEvent } from "react";
+import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
-}
+  name: string;
+  setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void;
+  addUser: () => void;
+  error: string;
+  totalUsers: number;
+};
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+  { name, setNameCallback, addUser, error, totalUsers } // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+  const inputClass = error ? s.error : "";
 
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    )
-}
+  return (
+    <div>
+      <input value={name} onChange={setNameCallback} className={inputClass} />
+      <span>{error}</span>
+      <button onClick={addUser}>add</button>
+      <span>{totalUsers}</span>
+    </div>
+  );
+};
 
-export default Greeting
+export default Greeting;
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,38 +1,49 @@
-import React, {useState} from 'react'
-import Greeting from './Greeting'
+import React, { useState, ChangeEvent } from "react";
+import Greeting from "./Greeting";
+import { UserType } from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
-}
+  users: Array<UserType>;
+  addUserCallback: (name: string) => void;
+};
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
 // более современный и удобный для про :)
 // уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+  users,
+  addUserCallback,
+}) => {
+  // деструктуризация пропсов
+  const [name, setName] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+  const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+  };
+  const addUser = () => {
+    if (name && name.length > 0) {
+      addUserCallback(name);
+      setName("");
+      setError("");
+    } else {
+      setError("Please enter not empty string");
     }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
-    }
+  };
 
-    const totalUsers = 0 // need to fix
+  const totalUsers = users.length;
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    )
-}
+  return (
+    <Greeting
+      name={name}
+      setNameCallback={setNameCallback}
+      addUser={addUser}
+      error={error}
+      totalUsers={totalUsers}
+    />
+  );
+};
 
-export default GreetingContainer
+export default GreetingContainer;
